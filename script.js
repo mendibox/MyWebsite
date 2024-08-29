1b6f11ec70e67ea4e850f02b50e8c70597b769dc
Juan Luis Mendiola <mendibox@gmail.com>
2024-08-29 16:29:03 -0600

    checking screen size

--- script.js
+++ script.js
@@ -9,8 +9,10 @@ function toggleMenu() {
 
 function showers () {
     let showersTaken = 6; //Real number of showers taken since August 2024
-    let litersSaved = 100; //Need confirmation
-    let result = showersTaken * litersSaved;
+    let unconsciousLiters = 93; //Average liters quantity in a single shower without any kind of consciousness
+    let semiConsciousLiters = 0; //Pending
+    let consciousLiters = 20 //Liters spend with an extreme consciousness
+    let result = (unconsciousLiters - consciousLiters) * 6;
     let element = document.getElementById('liters');
     element.innerHTML = result;
 }
