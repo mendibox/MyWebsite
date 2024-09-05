3d6ec78a64acf1a771fefa82b033e32614b74754
Juan Luis Mendiola <mendibox@gmail.com>
2024-09-05 12:27:27 -0600

    implement js function to toggle hero image

--- README.md
+++ README.md
@@ -1,21 +1,29 @@
 # Overview
 
 This is my personal website project. I'm actively learning software development and I've decided to practice all the concepts, technologies and tools with this essential project.
+This is a "work in progress" project and I will be modifying it through my journey.
 
-Of course, this is a "work in progress" project and I will be modifying it through my journey.
+## Technologies and tools used to develop this project
 
-# Technologies and tools used to develop this project
+### HTML
+- Elements
+- Attributes
+- Semantic elements
+### CSS
+- Type selectors
+- ID selectors
+- Class selectors
+- Pseudo-selectors
+- Global selector
+- pseudo-elements
 
-- HTML
-- CSS
-- Markdown
+### Markdown 
 - Git
 - Linux shell
 - Visual Studio Code
 - Google Chrome browser
 
-# Learning resources
-
+## Learning resources
 - W3Schools
 - freeCodeCamp
 - Font Awesome
--- script.js
+++ script.js
@@ -8,31 +8,18 @@ function toggleMenu() {
 }
 
 function toggleHero() {
-    var about = document.getElementById('about');
-    var aboutStyles = window.getComputedStyle(about);
-    var aboutWidth = aboutStyles.getPropertyValue('width');
-    let heroImage = document.getElementById('hero');
-    //let heroStyles = window.getComputedStyle(heroImage);
-    //let heroAttribute = heroImage.getAttribute('src');
-    
-    
-    //alert(aboutWidth);
-    if(aboutWidth >= '481px'){
-        heroImage.setAttribute('src', './img/myPicture.jpg');
-        //eroStyles.setProperty('width', 'fit-content');
-        //aboutStyles.setProperty()
-    } else if (aboutWidth <= '480px'){
-        heroImage.setAttribute('src', './img/mine.png');
+    var heroElement = document.getElementById('hero');
+    var heroStyles = window.getComputedStyle(heroElement);
+    var heroWidth = heroStyles.getPropertyValue('width');
+    let heroSource = document.getElementById('hero');
+    if (heroWidth <= '109px') {
+        heroSource.setAttribute('src', './img/mine.png');
+    } else {
+        heroSource.setAttribute('src', './img/myPicture.jpg');
     }
-    /*
-    
-    
-    if(heroAttribute === './img/mine.png'){
-        
-    }*/
 }
 
-function activarMenu(){
+function activarMenu() {
     let iconoMenu = document.getElementById('contenedor-menu');
     if (iconoMenu.style.display === 'block') {
         iconoMenu.style.display = 'none';
@@ -41,7 +28,7 @@ function activarMenu(){
     }
 }
 
-function showers () {
+function showers() {
     let showersTaken = 7; //Real number of showers taken since August 2024
     let unconsciousLiters = 93; //Average liters quantity in a single shower without any kind of consciousness
     let semiConsciousLiters = 41; //Mid consciousness
