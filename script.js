bf5b96afb4559edef1d34058c358c3271f31516b
Juan Luis Mendiola <mendibox@gmail.com>
2024-10-03 19:03:14 -0600

    incremet js

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ function activarMenu() {
 }
 
 function showers() {
-    let showersTaken = 19; //Real number of showers taken since August 2024
+    let showersTaken = 20; //Real number of showers taken since August 2024
     let unconsciousLiters = 93; //Average liters quantity in a single shower without any kind of consciousness
     let semiConsciousLiters = 41; //Mid consciousness
     let consciousLiters = 20 //Liters spend with an extreme consciousness
