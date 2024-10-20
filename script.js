68ab6d2c1544000d35593b9b1d1e060fba0fd896
Juan Luis Mendiola <mendibox@gmail.com>
2024-10-19 23:11:07 -0600

    removing cursor

--- script.js
+++ script.js
@@ -14,24 +14,15 @@ function currentSlide(n) {
 function showSlides(n) {
     let i;
     let slides = document.getElementsByClassName("slides");
-    let dots = document.getElementsByClassName("dot");
-
     if (n > slides.length) {
         slideIndex = 1;
     }
-
     if (n < 1) {
         slideIndex = slides.length;
     }
-
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-
     slides[slideIndex - 1].style.display = "block";
 }
 
@@ -43,11 +34,6 @@ function toggleMainMenu() {
     } else {
         mainMenu.style.display = 'block';
     }
-}
-
-function closeMainMenu() {
-    let menuOption = document.getElementById('menu-container')
-
 }
 
 function showers() {
